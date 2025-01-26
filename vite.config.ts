c4e55b25591d37f8888ed6fef5af49bbb3562854
Wisdom Olubayo <140736928+wizzyszn@users.noreply.github.com>
2025-01-27 00:06:09 +0100

    Wizzy (#1)
    * v0
    
    * initialized

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: '0.0.0.0', // Bind to all IPs
+    port: 5173,       // Specify your desired port (default is 5173)
+  },
+})
